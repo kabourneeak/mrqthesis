6ff7670939447d5cd27349055bf78379b7a8562c
Gregory Bint <greg@kabourneeak.net>
2014-11-18 11:45:01 -0500

    consistency checking

---
+++ check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Checking consistency"
+
+echo
+echo "\\BigOh"
+for f in *.tex; do grep -Hn "O(" $f; done
+for f in *.tex; do grep -Hn "\\\\BigOh(" $f; done
+
+echo
+echo "\\in_\\rho"
+for f in *.tex figures/*.ipe; do grep -Hn "\\in_rho" $f; done
+
+
