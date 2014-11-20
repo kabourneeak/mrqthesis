e87180c2a62a54a68fb97f197d764a02b03817d7
Gregory Bint <greg@kabourneeak.net>
2014-11-20 17:04:04 -0500

    WIP

--- check.sh
+++ check.sh
@@ -11,4 +11,8 @@ echo
 echo "\\in_\\rho"
 for f in *.tex figures/*.ipe; do grep -Hn "\\in_rho" $f; done
 
+echo
+echo "half-plane"
+for f in *.tex; do grep -Hn "half plane" $f; done
+for f in *.tex; do grep -Hn "halfplane" $f; done
 
